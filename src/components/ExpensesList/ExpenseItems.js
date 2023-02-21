7526a90a5e0f0aeebc3f1876daa19fd18ef5c0e9
PIvSky <piotr.iwansky@gmail.com>
2023-02-21 17:51:20 +0100

    problem with deleting items resolved

--- src/components/ExpensesList/ExpenseItems.js
+++ src/components/ExpensesList/ExpenseItems.js
@@ -4,26 +4,31 @@ import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItems.css';
 
-const container = document.querySelector('.expense-item');
 
 const onTrash = () => {
-  container.remove(container.children);
-  // console.log(e)
+  // const container = document.querySelector('.container');
+  const container = document.querySelector('.expense-item');
+  container.remove(container.childElement);
+  // container.removeChild(container)
+  // container?.removeChild(container)
+  // console.log(container.nodeValue)
 };
 
 function ExpenseItem(props) {
 
   return (
-    <Card className='expense-item'  >
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'  >
-        <h2>{props.title}</h2>
-        <div className='expense-item__price' >{props.amount + '$' }</div>
-      </div>
-      <button type="button" className='expense-item__trash' onClick={onTrash} >
-        {/* <img className='trash' src='/src/icons/trashIcon.png' alt='trashIcon'></img> */}
-      </button>
-    </Card>
+    <div>
+      <Card className='expense-item'  >
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'  >
+          <h2>{props.title}</h2>
+          <div className='expense-item__price' >{props.amount + '$' }</div>
+        </div>
+        <button type="button" className='expense-item__trash' onClick={onTrash} >
+          {/* <img className='trash' src='/src/icons/trashIcon.png' alt='trashIcon'></img> */}
+        </button>
+      </Card>
+    </div>
     );
 };
 
